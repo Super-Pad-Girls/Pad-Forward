16ab5fd659a8c1fc90e4960f175f57ec99b83348
Younghyun Lee <younghyunlee22@gmail.com>
2024-02-03 13:13:15 -0600

    Google Maps API

--- client/src/pages/BecomeADonator.jsx
+++ client/src/pages/BecomeADonator.jsx
@@ -1,6 +1,36 @@
-import './BecomeADonator.scss';
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const BecomeADonator = () => {
-    return <>BecomeADonator</>;
+const containerStyle = {
+  width: '400px',
+  height: '400px'
 };
-export default BecomeADonator;
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function BecomeADonator() {
+  // const API_KEY = process.env.REACT_APP_API_GOOGLE_MAPS_API
+
+  return (
+    <>
+    <LoadScript
+      googleMapsApiKey={`${import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY}`}// Replace with your Google Maps API key
+    >
+      
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, like markers, info windows, etc. go here */ }
+      </GoogleMap>
+    </LoadScript>
+    </>
+    
+  )
+}
+
+export default React.memo(BecomeADonator);

897bca1a908adeff8371093f8eed90f7cc59f367
Younghyun Lee <younghyunlee22@gmail.com>
2024-02-03 16:06:12 -0600

    feat: google_maps_buttons

--- client/src/pages/BecomeADonor.jsx
+++ client/src/pages/BecomeADonor.jsx
@@ -1,19 +1,52 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useEffect, useState } from "react"
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import "./BecomeADonor.scss";
 
 const containerStyle = {
-    width: "400px",
+    width: "800px",
     height: "400px",
 };
 
-const center = {
-    lat: -3.745,
-    lng: -38.523,
+const chicago = {
+    lat: 41.894,
+    lng: -87.624,
 };
 
+const sanFran = {
+  lat: 37.800,
+  lng: -122.438,
+};
+
+const michiganCity = {
+  lat: 41.719,
+  lng: -86.902,
+};
+
+const seattle = {
+  lat: 47.614,
+  lng: -122.328,
+};
+
+
 function BecomeADonor() {
+  const [selectedLocation, setSelectedLocation] = useState(chicago);
+
+  const handleClickLocation = (location) => {
+    setSelectedLocation(location);
+  };
+
+  useEffect(() => {
+  }, [selectedLocation]);
+
     return (
         <>
+        <div className="state-buttons">
+          <button onClick={() => handleClickLocation(chicago)} className="btn btn-primary"> Illinois </button> {" "}
+          <button onClick={() => handleClickLocation(michiganCity)} className="btn btn-primary"> Indiana </button> {" "}
+          <button onClick={() => handleClickLocation(sanFran)} className="btn btn-primary"> California </button> {" "}
+          <button onClick={() => handleClickLocation(seattle)} className="btn btn-primary"> Washington </button> {" "}
+        </div>
+        <div className="map-box">
             <LoadScript
                 googleMapsApiKey={`${
                     import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY
@@ -21,10 +54,13 @@ function BecomeADonor() {
             >
                 <GoogleMap
                     mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={10}
-                ></GoogleMap>
+                    center={selectedLocation}
+                    zoom={15}
+                >
+                  <MarkerF position={selectedLocation} icon={{url: '/images/marker_icon.png', scale: 5}} />
+                </GoogleMap>
             </LoadScript>
+            </div>
         </>
     );
 }
